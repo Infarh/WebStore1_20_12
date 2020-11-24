bf4b5ff04d6a2741238f634897e633e0fb8ed503
Infarh <shmachilin@yandex.ru>
2020-11-24 20:30:36 +0300

    Первый контроллер

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _Configuration;
+
+        public HomeController(IConfiguration Configuration) => _Configuration = Configuration;
+
+        public IActionResult Index()
+        {
+            // Обработка данных
+
+            return Content(_Configuration["ControllerActionText"]);
+        }
+
+        public IActionResult SecondAction()
+        {
+            return Content("Second controller action");
+        }
+    }
+}
