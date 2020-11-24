3a6aaf777d1c92c1ed2256d1d374075c889160a7
Infarh <shmachilin@yandex.ru>
2020-11-24 20:49:09 +0300

    Передача данных на представление в виде модели

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.Extensions.Configuration;
+
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IConfiguration _Configuration;
+
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 26 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 35 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 15 },
+        };
 
         public HomeController(IConfiguration Configuration) => _Configuration = Configuration;
 
@@ -20,6 +32,11 @@ namespace WebStore.Controllers
         public IActionResult SecondAction()
         {
             return Content("Second controller action");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(__Employees);
         }
     }
 }
