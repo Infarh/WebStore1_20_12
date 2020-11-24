f8dcbbf4fae4f6f50e3aba7f9c4c38dcbb8a9d6b
Infarh <shmachilin@yandex.ru>
2020-11-24 20:26:04 +0300

    Добавление логики инфраструктуры MVC в приложене

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,6 +16,7 @@ namespace WebStore
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -24,14 +26,22 @@ namespace WebStore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync(_Configuration["greetings"]);
-                });
+                endpoints.MapGet("/greetings", async ctx => await ctx.Response.WriteAsync(_Configuration["greetings"]));
+                endpoints.MapGet("/HelloWorld", async ctx => await ctx.Response.WriteAsync("Hello World!"));
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                // http://localhost:5000 -> controller == "Home" action == "Index"
+                // http://localhost:5000/Products -> controller == "Products" action == "Index"
+                // http://localhost:5000/Products/Page -> controller == "Products" action == "Page"
+                // http://localhost:5000/Products/Page/5 -> controller == "Products" action == "Page" id = "5"
             });
         }
     }
