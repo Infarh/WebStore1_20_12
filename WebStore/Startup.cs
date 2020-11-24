6489eacf443c555ca9d95926eab85db56bcd4893
Infarh <shmachilin@yandex.ru>
2020-11-24 21:05:50 +0300

    Динамическая компиляция представлений

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -16,7 +16,7 @@ namespace WebStore
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
