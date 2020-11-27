40023b01f231a48c44394ef9534285f359899aba
Infarh <shmachilin@yandex.ru>
2020-11-27 21:09:09 +0300

    Контроллер сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,23 +1,12 @@
-﻿using System.Collections.Generic;
-
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.CSharp;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-
-using WebStore.Models;
+using WebStore.Data;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IConfiguration _Configuration;
-
-        private static readonly List<Employee> __Employees = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 26 },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 35 },
-            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 15 },
-        };
 
         public HomeController(IConfiguration Configuration) => _Configuration = Configuration;
 
@@ -30,7 +19,7 @@ namespace WebStore.Controllers
 
         public IActionResult Employees()
         {
-            return View(__Employees);
+            return View(TestData.Employees);
         }
 
         public IActionResult Blogs() => View();
---
+++ WebStore/Data/TestData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public static class TestData
+    {
+        public static List<Employee> Employees { get; } = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 26 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 35 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 15 },
+        };
+    }
+}
