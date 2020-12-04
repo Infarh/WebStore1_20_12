b76cf1f7d86c3d7d60daa0c35912c0c97e36f77f
Infarh <shmachilin@yandex.ru>
2020-12-04 20:54:00 +0300

    Визуальный компонент, формирующий список секций в боковой панели

---
+++ WebStore/ViewModels/SectionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using WebStore.Domain.Entityes.Base.Interfaces;
+
+namespace WebStore.ViewModels
+{
+    public class SectionViewModel : INamedEntity, IOrderedEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Order { get; set; }
+
+        public List<SectionViewModel> ChildSections { get; set; }
+
+        public SectionViewModel ParentSection { get; set; }
+    }
+}
