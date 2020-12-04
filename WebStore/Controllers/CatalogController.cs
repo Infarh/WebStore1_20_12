949b8e299e7cced936fe366fd1362982a69b8a1c
Infarh <shmachilin@yandex.ru>
2020-12-04 21:42:10 +0300

    Контроллер каталога товаров

---
+++ WebStore/Controllers/CatalogController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData _ProductData;
+
+        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
+
+        public IActionResult Shop(int? BrandId, int? SectionId)
+        {
+            var filter = new ProductFilter
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+            };
+
+            var products = _ProductData.GetProducts(filter);
+
+            return View(new CatalogViewModel
+            {
+                SectionId = SectionId,
+                BrandId = BrandId,
+                Products = products
+                   .OrderBy(p => p.Order)
+                   .Select(p => new ProductViewModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Price = p.Price,
+                        ImageUrl = p.ImageUrl
+                    })
+            });
+        }
+    }
+}
--- WebStore/ViewModels/ProductViewModel.cs
+++ WebStore/ViewModels/ProductViewModel.cs
@@ -2,11 +2,10 @@
 
 namespace WebStore.ViewModels
 {
-    public class ProductViewModel : INamedEntity, IOrderedEntity
+    public class ProductViewModel : INamedEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Order { get; set; }
         public string ImageUrl { get; set; }
         public decimal Price { get; set; }
     }
