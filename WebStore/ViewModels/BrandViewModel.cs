19bcbac5b1797c92c8859b8657b587ee6d9afe61
Infarh <shmachilin@yandex.ru>
2020-12-04 21:03:43 +0300

    Визуальный компонент брендов

---
+++ WebStore/ViewModels/BrandViewModel.cs
@@ -0,0 +1,11 @@
+﻿using WebStore.Domain.Entityes.Base.Interfaces;
+
+namespace WebStore.ViewModels
+{
+    public class BrandViewModel : INamedEntity
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+    }
+}
--- WebStore/ViewModels/SectionViewModel.cs
+++ WebStore/ViewModels/SectionViewModel.cs
@@ -11,7 +11,7 @@ namespace WebStore.ViewModels
 
         public int Order { get; set; }
 
-        public List<SectionViewModel> ChildSections { get; set; }
+        public List<SectionViewModel> ChildSections { get; set; } = new();
 
         public SectionViewModel ParentSection { get; set; }
     }
