d692c6d0304b9cd23e217ef81f189a476ebbdf74
Infarh <shmachilin@yandex.ru>
2020-12-04 21:15:32 +0300

    Модели-представления каталога товаров

---
+++ WebStore/ViewModels/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using WebStore.Domain.Entityes.Base.Interfaces;
+
+namespace WebStore.ViewModels
+{
+    public class ProductViewModel : INamedEntity, IOrderedEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
