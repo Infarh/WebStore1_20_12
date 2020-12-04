512a21102b6c232685823ca75644d1670c1da9c9
Infarh <shmachilin@yandex.ru>
2020-12-04 20:26:49 +0300

    Сущности предметной области

---
+++ WebStore.Domain/Entityes/Section.cs
@@ -0,0 +1,12 @@
+﻿using WebStore.Domain.Entityes.Base;
+using WebStore.Domain.Entityes.Base.Interfaces;
+
+namespace WebStore.Domain.Entityes
+{
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        public int? ParentId { get; set; }
+    }
+}
