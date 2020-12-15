f598ba0cc19960ad1038a432545fdaa41c1918bc
Infarh <shmachilin@yandex.ru>
2020-12-15 19:23:05 +0300

    Минификация стилей и скриптов

--- WebStore/wwwroot/js/contact.js
+++ WebStore/wwwroot/js/contact.js
@@ -1,9 +1,7 @@
 jQuery(function ($) {
-    'use strict';
 
     var form = $('.contact-form');
     form.submit(function () {
-        'use strict';
 
         $this = $(this);
         $.post("sendemail.php", $(".contact-form").serialize(), function (result) {
