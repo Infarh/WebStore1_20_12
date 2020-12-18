10cd14912c85b89812af40cd268495c8155c4386
Infarh <shmachilin@yandex.ru>
2020-12-18 21:28:02 +0300

    Профиль пользователя

---
+++ WebStore/Controllers/UserProfileController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public async Task<IActionResult> Orders([FromServices] IOrderService OrderService)
+        {
+            var orders = await OrderService.GetUserOrders(User.Identity!.Name);
+
+            return View(orders.Select(order => new UserOrderViewModel(
+                order.Id, 
+                order.Name, 
+                order.Phone, 
+                order.Address, 
+                order.Items.Sum(item => item.Price * item.Quantity)
+                )));
+        }
+    }
+}
---
+++ WebStore/ViewModels/UserOrderViewModel.cs
@@ -0,0 +1,4 @@
+﻿namespace WebStore.ViewModels
+{
+    public record UserOrderViewModel(int Id, string Name, string Phone, string Address, decimal TotalSum);
+}
