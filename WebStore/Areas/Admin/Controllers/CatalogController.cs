f6f52faeaa7be1a2dbc7cbae1684ece69505bee0
Infarh <shmachilin@yandex.ru>
2020-12-18 20:19:27 +0300

    Логика работы с товарами в админке

--- WebStore/Areas/Admin/Controllers/CatalogController.cs
+++ WebStore/Areas/Admin/Controllers/CatalogController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Identity;
 using WebStore.Infrastructure.Interfaces;
 
@@ -12,6 +15,39 @@ namespace WebStore.Areas.Admin.Controllers
 
         public CatalogController(IProductData ProductData) => _ProductData = ProductData;
 
-        public IActionResult Index() => View();
+        public IActionResult Index() => View(_ProductData.GetProducts());
+
+        public IActionResult Edit(int id)
+        {
+            var product = _ProductData.GetProductById(id);
+            if (product is null) return NotFound();
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (!ModelState.IsValid) return View(product);
+            
+            // Логика редактирования
+            // Логика вызова метода из IProductData
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _ProductData.GetProductById(id);
+            if (product is null) return NotFound();
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+        {
+            // Логика удаления
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
