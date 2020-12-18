f11d7864bb6d65f5eeb6180f1f9eba40bf5a301b
Infarh <shmachilin@yandex.ru>
2020-12-18 19:46:52 +0300

    Настройка шаблона области администратора

---
+++ WebStore/Areas/Admin/Controllers/CatalogController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CatalogController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
