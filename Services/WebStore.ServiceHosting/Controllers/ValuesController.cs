6c9e8452ec7ff7b2c40c397d5a77cdc4018b9c43
Infarh <shmachilin@yandex.ru>
2021-01-16 17:30:49 +0300

    Тeстовое WebAPI

---
+++ Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/[controller]")] // http://localhost:5001/api/values
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly List<string> __Values = Enumerable
+           .Range(1, 10)
+           .Select(i => $"Value{i:00}")
+           .ToList();
+
+        [HttpGet] // http://localhost:5001/api/values
+        public IEnumerable<string> Get() => __Values;
+
+        [HttpGet("{id}")] // http://localhost:5001/api/values/5
+        public ActionResult<string> Get(int id)
+        {
+            if (id < 0)
+                return BadRequest();
+            if (id >= __Values.Count)
+                return NotFound();
+
+            return __Values[id];
+        }
+
+        [HttpPost]
+        [HttpPost("add")] //http://localhost:5001/api/values/add
+        public ActionResult Post([FromBody] string value)
+        {
+            __Values.Add(value);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        [HttpPut("edit/{id}")] //http://localhost:5001/api/values/edit/5
+        public ActionResult Put(int id, [FromBody] string value)
+        {
+            if (id < 0)
+                return BadRequest();
+            if (id >= __Values.Count)
+                return NotFound();
+
+            __Values[id] = value;
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (id < 0)
+                return BadRequest();
+            if (id >= __Values.Count)
+                return NotFound();
+
+            __Values.RemoveAt(id);
+
+            return Ok();
+        }
+    }
+}
---
+++ Services/WebStore.ServiceHosting/Startup.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace WebStore.ServiceHosting
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebStore.ServiceHosting", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebStore.ServiceHosting v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
