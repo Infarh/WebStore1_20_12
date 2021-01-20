9b3e4b68238875b27713eb4cef75312cf3b5c5c8
Infarh <shmachilin@yandex.ru>
2021-01-20 19:55:32 +0300

    Заготовка клиента для WebAPI управления сотрудниками

---
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using WebStore.Clients.Base;
+using WebStore.Domain.Models;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        private readonly ILogger<EmployeesClient> _Logger;
+
+        public EmployeesClient(IConfiguration Configuration, ILogger<EmployeesClient> Logger)
+            : base(Configuration, "api/employees") =>
+            _Logger = Logger;
+
+
+        public IEnumerable<Employee> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
