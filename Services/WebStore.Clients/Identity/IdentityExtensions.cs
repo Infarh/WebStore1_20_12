62c9f7f6389fbf7f776ecaf9391cd130812ba3d0
Infarh <shmachilin@yandex.ru>
2021-01-23 17:40:15 +0300

    Удаление БД из главного приложения

---
+++ Services/WebStore.Clients/Identity/IdentityExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Clients.Identity
+{
+    public static class IdentityExtensions
+    {
+        public static IServiceCollection AddIdentityWebStoreWebAPIClients(this IServiceCollection services)
+        {
+            services
+               .AddTransient<IUserStore<User>, UsersClient>()
+               .AddTransient<IUserRoleStore<User>, UsersClient>()
+               .AddTransient<IUserPasswordStore<User>, UsersClient>()
+               .AddTransient<IUserEmailStore<User>, UsersClient>()
+               .AddTransient<IUserPhoneNumberStore<User>, UsersClient>()
+               .AddTransient<IUserTwoFactorStore<User>, UsersClient>()
+               .AddTransient<IUserClaimStore<User>, UsersClient>()
+               .AddTransient<IUserLoginStore<User>, UsersClient>();
+
+            services.AddTransient<IRoleStore<Role>, RolesClient>();
+
+            return services;
+        }
+
+        public static IdentityBuilder AddIdentityWebStoreWebAPIClients(this IdentityBuilder builder)
+        {
+            builder.Services.AddIdentityWebStoreWebAPIClients();
+            return builder;
+        }
+    }
+}
