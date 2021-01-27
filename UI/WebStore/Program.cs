745e13e1ddd59b3df8ec60ab39dab651c5e293cc
Infarh <shmachilin@yandex.ru>
2021-01-27 19:52:51 +0300

    Конфигурация системы логирования

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace WebStore
 {
@@ -11,6 +12,13 @@ namespace WebStore
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(host => host.UseStartup<Startup>());
+                .ConfigureWebHostDefaults(host => host.UseStartup<Startup>())
+                //.ConfigureLogging((host, log) => log
+                //    .ClearProviders()
+                //    .AddEventLog(opt => opt.LogName = "WebStore.log")
+                //    .AddFilter("Microsoft.Hosting", LogLevel.Error)
+                //    .AddFilter((category, level) => !(category.StartsWith("Microsoft") && level >= LogLevel.Warning))
+                // )
+            ;
     }
 }
