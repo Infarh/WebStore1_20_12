4a82519a01d99b28bd5000c47c3605ee920ddea0
Infarh <shmachilin@yandex.ru>
2021-01-27 20:24:17 +0300

    Метод-расширение для добавления логирования в log4NET

---
+++ Common/WebStore.Logger/Log4NetLoggerFactoryExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Logger
+{
+    public static class Log4NetLoggerFactoryExtensions
+    {
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory Factory, string ConfigurationFile = "log4net.config")
+        {
+            if (ConfigurationFile is null) throw new ArgumentNullException(nameof(ConfigurationFile));
+
+            if (!Path.IsPathRooted(ConfigurationFile))
+            {
+                var assembly = Assembly.GetEntryAssembly() ?? throw new InvalidOperationException("Не удалось определить сборку, содержащую точку входа в приложение");
+                var dir = Path.GetDirectoryName(assembly.Location) ?? throw new InvalidOperationException("Получена пустая ссылка на строку пути к сборке с точкой входа в приложение");
+                ConfigurationFile = Path.Combine(dir, ConfigurationFile);
+            }
+
+            Factory.AddProvider(new Log4NetLoggerProvider(ConfigurationFile));
+
+            return Factory;
+        }
+    }
+}
