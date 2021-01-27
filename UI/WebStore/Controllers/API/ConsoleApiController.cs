770c414e22b0d298da80a025e58f328059de2a67
Infarh <shmachilin@yandex.ru>
2021-01-27 21:08:13 +0300

    Добавление логгров в программу

---
+++ UI/WebStore/Controllers/API/ConsoleApiController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers.API
+{
+    [Route("api/console")]
+    [ApiController]
+    public class ConsoleApiController : ControllerBase
+    {
+        [HttpGet("clear")]
+        public void Clear() => Console.Clear();
+
+        [HttpGet("writeline/{str}")]
+        public void WriteLine(string str) => Console.WriteLine(str);
+    }
+}
--- UI/WebStore/Controllers/AccountController.cs
+++ UI/WebStore/Controllers/AccountController.cs
@@ -1,8 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.Logging;
 using WebStore.Domain.Entities.Identity;
 using WebStore.Domain.ViewModels;
 
@@ -13,11 +14,16 @@ namespace WebStore.Controllers
     {
         private readonly UserManager<User> _UserManager;
         private readonly SignInManager<User> _SignInManager;
+        private readonly ILogger<AccountController> _Logger;
 
-        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        public AccountController(
+            UserManager<User> UserManager, 
+            SignInManager<User> SignInManager,
+            ILogger<AccountController> Logger)
         {
             _UserManager = UserManager;
             _SignInManager = SignInManager;
+            _Logger = Logger;
         }
 
         #region Регистрация нового пользователя
@@ -32,22 +38,40 @@ namespace WebStore.Controllers
             if (!ModelState.IsValid)
                 return View(Model);
 
-            var user = new User
-            {
-                UserName = Model.UserName
-            };
+            _Logger.LogInformation("Регистрация нового пользователя {0}", Model.UserName);
+            //_Logger.LogInformation($"Регистрация нового пользователя {Model.UserName}");
 
-            var registration_result = await _UserManager.CreateAsync(user, Model.Password);
-            if (registration_result.Succeeded)
+            using (_Logger.BeginScope("Регистрация пользователя {0}", Model.UserName))
             {
-                await _UserManager.AddToRoleAsync(user, Role.User);
-                
-                await _SignInManager.SignInAsync(user, false);
-                return RedirectToAction("Index", "Home");
+                var user = new User
+                {
+                    UserName = Model.UserName
+                };
+
+                var registration_result = await _UserManager.CreateAsync(user, Model.Password);
+                if (registration_result.Succeeded)
+                {
+                    _Logger.LogInformation("Пользователь {0} зарегистрирован", user.UserName);
+
+                    await _UserManager.AddToRoleAsync(user, Role.User);
+
+                    _Logger.LogInformation("Пользователю {0} назначена роль {1}",
+                        user.UserName, Role.User);
+
+
+                    await _SignInManager.SignInAsync(user, false);
+                    _Logger.LogInformation("Пользователь {0} автоматически вошёл в систему после регистрации", user.UserName);
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                _Logger.LogWarning("Ошибка при регистрации нового пользователя {0}:{1}",
+                    user.UserName,
+                    string.Join(",", registration_result.Errors.Select(e => e.Description)));
+
+                foreach (var error in registration_result.Errors)
+                    ModelState.AddModelError("", error.Description);
             }
-
-            foreach (var error in registration_result.Errors)
-                ModelState.AddModelError("", error.Description);
 
             return View(Model);
         }
@@ -65,6 +89,8 @@ namespace WebStore.Controllers
         {
             if (!ModelState.IsValid) return View(Model);
 
+            _Logger.LogInformation("Вход пользователя {0} в систему", Model.UserName);
+
             var login_result = await _SignInManager.PasswordSignInAsync(
                 Model.UserName,
                 Model.Password,
@@ -78,10 +104,15 @@ namespace WebStore.Controllers
 
             if (login_result.Succeeded)
             {
+                _Logger.LogInformation("Вход пользователя {0} в систему - успешно выполнен", Model.UserName);
+
                 if (Url.IsLocalUrl(Model.ReturnUrl))
                     return Redirect(Model.ReturnUrl);
                 return RedirectToAction("Index", "Home");
             }
+
+            _Logger.LogWarning("Вход пользователя {0} в систему - неверный пароль, или имя пользователя", Model.UserName);
+
 
             ModelState.AddModelError("", "Неверное имя пользователя, или пароль!");
 
@@ -92,11 +123,20 @@ namespace WebStore.Controllers
 
         public async Task<IActionResult> Logout()
         {
+            var user_name = User.Identity!.Name;
             await _SignInManager.SignOutAsync();
+
+            _Logger.LogInformation("{0} вышел из системы", user_name);
+
             return RedirectToAction("Index", "Home");
         }
 
         [AllowAnonymous]
-        public IActionResult AccessDenied() => View();
+        public IActionResult AccessDenied()
+        {
+            _Logger.LogWarning("Отказано в доступе к {0}", Request.Path);
+
+            return View();
+        }
     }
 }
